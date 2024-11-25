dd5507e3016cb3298ec1b2db225d0f5d8f15d3e7
anTon <138380708+r3kste@users.noreply.github.com>
2024-11-26 03:47:42 +0530

    initial SA

--- src/main.py
+++ src/main.py
@@ -3,8 +3,11 @@ from environment import Environment
 
 # The following import statement should be replaced with the correct import statement
 from algorithm_interface import PackingAlgorithm as PackingAlgorithm
+
 # For Example:
-from solvers.threeDBP_Pivoting import ThreeDBP_Pivoting as PackingAlgorithm
+from solvers.threeDBP_Pivoting import (
+    ThreeDBP_Pivoting_Simul_Annealing as PackingAlgorithm,
+)
 
 K = parser.get_K()
 uld_list = parser.get_uld_list()
@@ -18,3 +21,4 @@ model.solve(env)
 # env.plot()
 env.animate()
 env.summary()
+env.write()
