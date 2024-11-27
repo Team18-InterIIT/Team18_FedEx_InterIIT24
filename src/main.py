4328e0aa12291a7d83864f11de7ba26288588ecb
DragonEolf <krishnak1711@gmail.com>
2024-11-27 17:13:59 +0530

    made better

--- src/main.py
+++ src/main.py
@@ -7,10 +7,8 @@ from environment import Environment
 from algorithm_interface import PackingAlgorithm as PackingAlgorithm
 
 # For Example:
-from solvers.threeDBP_Pivoting import (
-    ThreeDBP_Pivoting_Simul_Annealing as PackingAlgorithm,
-)
-from solvers.Caving_COA import COA as PackingAlgorithm
+# from solvers.threeDBP_Pivoting import ThreeDBP_Pivoting as PackingAlgorithm
+from solvers.layerstratwithOR import LayerPacking
 
 if len(sys.argv) == 2:
     test_file = sys.argv[1]
@@ -24,14 +22,14 @@ pkg_list = parser.get_pkg_list()
 
 env = Environment(K, uld_list, pkg_list)
 
-model = PackingAlgorithm()
+model = LayerPacking()
 
 model.solve(env)
 # To read from a solution file, use the following line instead of the above line
 # env.read(file_path=f"solutions/{str(PackingAlgorithm.__name__)}/{test_file.split('/')[-1]}")
 
 env.summary()
-# env.plot()
+env.plot()
 # env.animate()
 env.write(
     file_path=f"solutions/{str(PackingAlgorithm.__name__)}/{test_file.split('/')[-1]}"
