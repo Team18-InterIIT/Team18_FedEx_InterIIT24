f239299fd220672c984ec14413369e8b63cffd26
Prithvi-Rao <ee22b024@smail.iitm.ac.in>
2024-11-28 15:47:40 +0530

    for srivastava to fix

--- src/main.py
+++ src/main.py
@@ -4,11 +4,11 @@ import parser
 from environment import Environment
 
 # The following import statement should be replaced with the correct import statement
-from algorithm_interface import PackingAlgorithm as PackingAlgorithm
+from solvers.layerstratwithOR import LayerPacking as PackingAlgorithm
 
 # For Example:
 # from solvers.threeDBP_Pivoting import ThreeDBP_Pivoting as PackingAlgorithm
-from solvers.layerstratwithOR import LayerPacking
+
 
 if len(sys.argv) == 2:
     test_file = sys.argv[1]
@@ -22,7 +22,7 @@ pkg_list = parser.get_pkg_list()
 
 env = Environment(K, uld_list, pkg_list)
 
-model = LayerPacking()
+model = PackingAlgorithm()
 
 model.solve(env)
 # To read from a solution file, use the following line instead of the above line
