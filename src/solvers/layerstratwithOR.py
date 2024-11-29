f6abecfb75fc53617494ff166f81d0a1c7a476ad
Prithvi-Rao <ee22b024@smail.iitm.ac.in>
2024-11-29 15:56:37 +0530

    lol

--- src/solvers/layerstratwithOR.py
+++ src/solvers/layerstratwithOR.py
@@ -20,49 +20,6 @@ class LayerPacking(PackingAlgorithm):
             print(layer.packing_eff,layer.cost,layer.dim.h)
         
 
-        #Knapsack on heights of layers to maximize the total value of packed items.
-        # solver = pywraplp.Solver.CreateSolver("SAT")
-        # if solver is None:
-        #     print("SCIP solver unavailable.")
-        #     return
-        # # Variables
-        # x = {}
-        # for i in range(len(layers)):
-        #     for b in range(len(env.ULDs)):
-        #         x[i, b] = solver.BoolVar(f"x_{i}_{b}")
-        
-        # # Constraints.
-        # # Each item is assigned to at most one bin.
-        # for i in range(len(layers)):
-        #     solver.Add(sum(x[i, b] for b in range(len(env.ULDs))) <= 1)
-
-        # # The amount packed in each bin cannot exceed its capacity.
-        # for b in range(len(env.ULDs)):
-        #     solver.Add(
-        #     sum(x[i, b] * layers[i].dim.h for i in range(len(layers)))
-        #     <= env.ULDs[b].dim.h
-        #     )
-
-        # # Objective.
-        # # Maximize total value of packed items.
-        # objective = solver.Objective()
-        # for i in range(len(layers)):
-        #     for b in range(len(env.ULDs)):
-        #         objective.SetCoefficient(x[i, b], layers[i].cost)
-        # objective.SetMaximization()
-        # print(len(layers),len(env.ULDs))
-        # print(f"Solving with {solver.SolverVersion()}")
-        # print(solver.NumVariables(), "variables")
-        # print(solver.NumConstraints(), "constraints")
-        # status = solver.Solve()
-        # print("status:",status) 
-        # mapping = [None]*len(env.ULDs)
-        # for i in range(len(layers)):
-        #     for b in range(len(env.ULDs)):
-        #         if x[i,b].solution_value() > 0:
-        #             if(mapping[b] == None):
-        #                 mapping[b] = []
-        #             mapping[b].append(layers[i])
         mapping = assign_layers(layers,env.ULDs,or_tools = True)
         if(mapping != None):
             for b in range(len(mapping)):
