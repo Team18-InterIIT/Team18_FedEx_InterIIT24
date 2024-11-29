0626e933b049e4100cc34753a7c49d99c38f2ed7
Prithvi-Rao <ee22b024@smail.iitm.ac.in>
2024-11-29 18:39:08 +0530

    setup

--- src/main.py
+++ src/main.py
@@ -30,7 +30,7 @@ model.solve(env)
 
 env.summary()
 # env.plot()
-env.animate()
+# env.animate()
 env.write(
     file_path=f"solutions/{str(PackingAlgorithm.__name__)}/{test_file.split('/')[-1]}"
 )
