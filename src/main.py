8e320833d449bcfb2f7f1d0adec947090e5dc2fb
DhruvJalan <75129140+Dhruvjalan@users.noreply.github.com>
2024-11-30 19:24:46 +0530

    Update main.py
    Changes in order to run from script directly

--- src/main.py
+++ src/main.py
@@ -1,19 +1,25 @@
 import sys
-
+import importlib
 import parser
 from environment import Environment
 
-# The following import statement should be replaced with the correct import statement
-from algorithm_interface import PackingAlgorithm as PackingAlgorithm
+# Ensure we have both test file and solver name arguments
+if len(sys.argv) != 3:
+    sys.exit(1)
 
-# For Example:
-# from solvers.threeDBP_Pivoting import ThreeDBP_Pivoting as PackingAlgorithm
+test_file = sys.argv[1]
+solver_module_name = sys.argv[2]
 
-if len(sys.argv) == 2:
-    test_file = sys.argv[1]
-else:
-    test_file = "test/Challenge_FedEx.txt"
+# Dynamically import the solver module
+try:
+    solver_module = importlib.import_module(solver_module_name)
+    print(solver_module)
+    PackingAlgorithm = solver_module.Algorithm
+except ModuleNotFoundError:
+    print(f"Error: Solver module '{solver_module_name}' not found.")
+    sys.exit(1)
 
+# Parse the test file
 parser = parser.Parser(test_file)
 K = parser.get_K()
 uld_list = parser.get_uld_list()
@@ -21,15 +27,15 @@ pkg_list = parser.get_pkg_list()
 
 env = Environment(K, uld_list, pkg_list)
 
+# Instantiate the algorithm and solve the problem
 model = PackingAlgorithm()
-
 model.solve(env)
-# To read from a solution file, use the following line instead of the above line
-# env.read(file_path=f"solutions/{str(PackingAlgorithm.__name__)}/{test_file.split('/')[-1]}")
 
+# Save the results to a solution file
+print("Printing Summary")
 env.summary()
 # env.plot()
-env.animate()
+# env.animate()
 env.write(
-    file_path=f"solutions/{str(PackingAlgorithm.__name__)}/{test_file.split('/')[-1]}"
+    file_path=f"solutions/{solver_module_name}/{test_file.split('/')[-1]}"
 )
